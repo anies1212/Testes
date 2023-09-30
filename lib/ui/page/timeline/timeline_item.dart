b47bd782588402773e78a6d1d4e509517b8c3138
b4tchkn <baaaakkrad@gmail.com>
2023-10-01 00:24:07 +0900

    feat: add audio player

--- lib/ui/page/timeline/timeline_item.dart
+++ lib/ui/page/timeline/timeline_item.dart
@@ -1,5 +1,7 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:decorated_icon/decorated_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hackathon_2023/foundation/audio_player/audio_player.dart';
 import 'package:flutter_hackathon_2023/model/post.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -15,6 +17,8 @@ class TimelineItem extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final audioPlayer = useAudioPlayer();
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -42,43 +46,50 @@ class TimelineItem extends HookConsumerWidget {
             ],
           ),
         ),
-        Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              clipBehavior: Clip.antiAlias,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                boxShadow: [
+        GestureDetector(
+          onTap: () {
+            audioPlayer.play(UrlSource(post.audioUrl));
+          },
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      spreadRadius: 2,
+                      blurRadius: 2,
+                      offset: Offset(3, 2),
+                    ),
+                  ],
+                ),
+                width: _imageSquareWidth,
+                height: _imageSquareWidth,
+                child: Image.network(
+                  'https://cdn.pixabay.com/photo/2013/07/18/15/07/hummingbird-164632__340.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              DecoratedIcon(
+                audioPlayer.state == PlayerState.playing
+                    ? Icons.pause_outlined
+                    : Icons.play_arrow,
+                color: Colors.white,
+                size: 32,
+                shadows: const [
                   BoxShadow(
-                    color: Colors.black26,
-                    spreadRadius: 2,
-                    blurRadius: 2,
-                    offset: Offset(3, 2),
+                    color: Colors.black12,
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: Offset(5, 5),
                   ),
                 ],
-              ),
-              width: _imageSquareWidth,
-              height: _imageSquareWidth,
-              child: Image.network(
-                'https://cdn.pixabay.com/photo/2013/07/18/15/07/hummingbird-164632__340.jpg',
-                fit: BoxFit.cover,
-              ),
-            ),
-            const DecoratedIcon(
-              Icons.play_arrow,
-              color: Colors.white,
-              size: 32,
-              shadows: [
-                BoxShadow(
-                  color: Colors.black12,
-                  spreadRadius: 5,
-                  blurRadius: 5,
-                  offset: Offset(5, 5),
-                ),
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ],
     );
