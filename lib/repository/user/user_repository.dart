52e78eb2ea505f519f6aa0733ec865143756a206
Yokoi-K <yokoanago@gmail.com>
2023-10-01 01:31:30 +0900

    feat: repository

--- lib/repository/user/user_repository.dart
+++ lib/repository/user/user_repository.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_hackathon_2023/model/user.dart';
 
 abstract class UserRepository {
-  Future<UserModel> findById(String id);
+  Future<UserModel> findById(int id);
 
   Future<int> create(String name);
 }
