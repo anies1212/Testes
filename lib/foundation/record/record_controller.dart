6d89eb6566d32f72e0e1e83f10a1c2d7d603ed72
b4tchkn <baaaakkrad@gmail.com>
2023-09-30 18:36:47 +0900

    feat: add record

---
+++ lib/foundation/record/record_controller.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_hackathon_2023/foundation/record/record_value.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:record/record.dart';
+
+RecordController useRecordController({
+  List<Object?> keys = const [],
+}) {
+  final controller = useMemoized(() => RecordController(RecordValue()));
+
+  useEffect(() => controller.dispose, keys);
+
+  return controller;
+}
+
+class RecordController extends ValueNotifier<RecordValue> {
+  RecordController(super.value);
+
+  final record = Record();
+
+  Future<void> startRecording() async {
+    final hasPermission = await record.hasPermission();
+    if (hasPermission) {
+      await record.start();
+      final isRecording = await _isRecording();
+      print('スタートした $isRecording');
+      value = RecordValue(isRecording: isRecording);
+    }
+  }
+
+  Future<String?> stopRecording() async {
+    final path = await record.stop();
+    final isRecording = await _isRecording();
+    print('ストップした $isRecording');
+    value = RecordValue(isRecording: isRecording);
+    return path;
+  }
+
+  @override
+  Future<void> dispose() {
+    super.dispose();
+    return record.dispose();
+  }
+
+  Future<bool> _isRecording() => record.isRecording();
+}
