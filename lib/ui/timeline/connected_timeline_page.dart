feb19c752f8b9c8122f56fd637c3aa7425110a3d
arakaki_seina <87635517+anies1212@users.noreply.github.com>
2023-09-30 22:04:16 +0900

    feat: divided into connected and page

--- lib/ui/timeline/connected_timeline_page.dart
+++ lib/ui/timeline/connected_timeline_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hackathon_2023/ui/page/app_background_container.dart';
-import 'package:flutter_hackathon_2023/ui/timeline/timeline_item.dart';
+import 'package:flutter_hackathon_2023/ui/timeline/timeline_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ConnectedTimelinePage extends HookConsumerWidget {
@@ -16,23 +16,9 @@ class ConnectedTimelinePage extends HookConsumerWidget {
         automaticallyImplyLeading: false,
         elevation: 0,
       ),
-      body: AppBackgroundContainer(
-          child: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate([
-              for (int i = 0; i < 10; i++)
-                const Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 18,
-                    horizontal: 24,
-                  ),
-                  child: TimelineItem(),
-                ),
-            ]),
-          ),
-        ],
-      )),
+      body: const AppBackgroundContainer(
+        child: TimelinePage(),
+      ),
     );
   }
 }
